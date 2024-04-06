eb8c968e6a24f17ed5ae4733a5a82f2e59949ea1
soumyadipjccn <mailsoumyadipghosh@gmail.com>
2024-04-06 09:28:18 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.logger import logging
 from src.exception import CustomException
 from sklearn.metrics import r2_score, mean_absolute_error,mean_squared_error
 import dill
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path, obj):
@@ -22,27 +23,33 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
-        for i in range(len(models)):
+
+        for i in range(len(list(models))):
             model = list(models.values())[i]
-            # Train model
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            
+            #model.fit(X_train, y_train)  # Train model
 
-            # Predict Testing data
-            y_test_pred =model.predict(X_test)
+            y_train_pred = model.predict(X_train)
 
-            # Get R2 scores for train and test data
-            #train_model_score = r2_score(ytrain,y_train_pred)
-            test_model_score = r2_score(y_test,y_test_pred)
+            y_test_pred = model.predict(X_test)
 
-            report[list(models.keys())[i]] =  test_model_score
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
     except Exception as e:
-        logging.info('Exception occured during model training')
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
