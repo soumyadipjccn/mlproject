3f208b57ad1533b5469849ed1884ca38ff939cf6
soumyadipjccn <mailsoumyadipghosh@gmail.com>
2024-04-08 09:06:37 +0530

    Some Update

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -67,49 +67,42 @@ class ModelTrainer:
 
 
 
-            params={
-                "Decision Tree": {
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
-                },
-                "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
-                    'learning_rate':[.1,.01,.05,.001],
-                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "Linear Regression":{},
-                "XGBRegressor":{
-                    'learning_rate':[.1,.01,.05,.001],
-                    'n_estimators': [8,16,32,64,128,256]
-                },
-                "CatBoosting Regressor":{
-                    'depth': [6,8,10],
-                    'learning_rate': [0.01, 0.05, 0.1],
-                    'iterations': [30, 50, 100]
-                },
-                "AdaBoost Regressor":{
-                    'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
-                    'n_estimators': [8,16,32,64,128,256]
-                }
-                
-            }
+#             params = {
+#     "Decision Tree": {
+#         'criterion': ['mse', 'mae'],
+#         'splitter': ['best', 'random'],
+#         'max_features': ['auto', 'sqrt', 'log2']
+#     },
+#     "Random Forest": {
+#         'n_estimators': [8, 16, 32, 64, 128, 256],
+#         'max_features': ['auto', 'sqrt', 'log2']
+#     },
+#     "Gradient Boosting": {
+#         'learning_rate': [.1, .01, .05, .001],
+#         'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+#         'max_features': ['auto', 'sqrt', 'log2'],
+#         'n_estimators': [8, 16, 32, 64, 128, 256]
+#     },
+#     "XGBRegressor": {
+#         'learning_rate': [.1, .01, .05, .001],
+#         'n_estimators': [8, 16, 32, 64, 128, 256]
+#     },
+#     "CatBoosting Regressor": {
+#         'depth': [6, 8, 10],
+#         'learning_rate': [0.01, 0.05, 0.1],
+#         'iterations': [30, 50, 100]
+#     },
+#     "AdaBoost Regressor": {
+#         'learning_rate': [.1, .01, 0.5, .001],
+#         'n_estimators': [8, 16, 32, 64, 128, 256]
+#     }
+# }
 
 
 
             # The evaluate_model() function is called with training and testing data along with the dictionary of models. It returns a dictionary containing evaluation metrics for each model.
 
-            model_report = evaluate_models(X_train, y_train, X_test, y_test, models, params)
+            model_report = evaluate_models(X_train, y_train, X_test, y_test, models)
             print(model_report)
             print('\n====================================================================================\n')
             logging.info(f'Model Report : {model_report}')
--- src/utils.py
+++ src/utils.py
@@ -23,31 +23,20 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train, y_train, X_test, y_test, models):
     try:
         report = {}
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
-
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
-
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-
-            #model.fit(X_train, y_train)  # Train model
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
-
             y_test_pred = model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
-
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
 
         return report
 
